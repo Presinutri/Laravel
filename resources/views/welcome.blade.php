ce07fb53071a6993fc2f378b044c6b81ead64e11
farizalbab <farizalbab@gmail.com>
2019-10-15 13:43:52 +0700

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fariz Albab
                 </div>
 
                 <div class="links">
